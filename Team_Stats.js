d891e1db9a84f4250c5351405b97cfceae6108ba
someguyfromwisconsin <ajordanmarcellino@gmail.com>
2023-12-16 10:29:53 -0600

    Add player and games properties

--- Team_Stats.js
+++ Team_Stats.js
@@ -5,22 +5,6 @@ After we create these data structures in this project, feel free to challenge yo
 */
 
 const team = {
-
+    _players: [],
+    _games: []
 };
-
-
-/*
-Populating Data Section
-
-1. Let’s make a data structure to store the information about our team. Declare a const variable called team and set it to an empty object.
-
-2. Our team has players and the games that they have played. Let’s represent both of these by adding two properties to your team object. Add a _players property and a _games property and initialize both to empty arrays.
-
-3. 
-
-
-Gathering Data Section
-
-
-
-Adding Data Section
