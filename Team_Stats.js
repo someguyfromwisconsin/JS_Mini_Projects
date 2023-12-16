b22f57e74e1c7df11ab27e60870feae962005724
someguyfromwisconsin <ajordanmarcellino@gmail.com>
2023-12-16 10:28:07 -0600

    Add empty structure to store team info

--- Team_Stats.js
+++ Team_Stats.js
@@ -4,15 +4,23 @@ We want to create, retrieve, and add information about your favorite sports team
 After we create these data structures in this project, feel free to challenge yourself to gain insights from them. For example, you might want to get the total number of games your team has played, or the average score of all of their games.
 */
 
-// Populating Data Section
+const team = {
+
+};
+
+
+/*
+Populating Data Section
+
+1. Let’s make a data structure to store the information about our team. Declare a const variable called team and set it to an empty object.
+
+2. Our team has players and the games that they have played. Let’s represent both of these by adding two properties to your team object. Add a _players property and a _games property and initialize both to empty arrays.
+
+3. 
+
+
+Gathering Data Section
 
 
 
-// Gathering Data Section
-
-
-
-// Adding Data Section
-
-
-
+Adding Data Section
